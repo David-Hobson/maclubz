933aa9fccf6206b201414863ed0a602109a2738c
David Hobson <davidhobson96@gmail.com>
2018-12-03 20:18:39 -0500

    Create search based on letters and category.

--- public/scripts/landing-search.js
+++ public/scripts/landing-search.js
@@ -1,22 +1,54 @@
 let letters = $(".letter-select");
-
+let category = $(".category");
+let teams = $(".teams");
 
 letters.each(function(index, letter){
 	$(letter).click(function(){
-		letters.removeClass("test");
-		$(this).addClass("test");
-	})
-});	
-
-
-let searchBox = $(".search-box");
-
-searchBox.keyup(function(){
-	// $(".landing-title").addClass("hide");
-	// $(".search-results").removeClass("hide");
-
-	// if(searchBox.val() == ""){
-	// 	$(".landing-title").removeClass("hide");
-	// 	$(".search-results").addClass("hide");
-	// }
+		letters.removeClass("highlight");
+		$(this).addClass("highlight");
+		letterSearch($(this).html());
+	});
 });
+
+category.change(function(){
+	categorySearch($(this).val());
+})
+
+
+function letterSearch(letter){
+	category.val("Select Category");
+	teams.each(function(index, team){
+		if(this.getAttribute("name").toLowerCase()[0] == letter.toLowerCase()){
+            $(this).removeClass("non-search");
+        }else {
+            $(this).addClass("non-search");
+        }
+	});
+
+	if(teams.length == $(".non-search").length){
+    	$(".no-results").removeClass("hide");
+    }else{
+    	$(".no-results").addClass("hide");
+    }
+}	
+
+function categorySearch(category){
+	letters.removeClass("highlight");
+	if(category != "Select Category"){
+		teams.each(function(index, team){
+			if(this.getAttribute("type").toLowerCase() == category.toLowerCase()){
+	            $(this).removeClass("non-search");
+	        }else {
+	            $(this).addClass("non-search");
+	        }
+		});
+
+		if(teams.length == $(".non-search").length){
+	    	$(".no-results").removeClass("hide");
+	    }else{
+	    	$(".no-results").addClass("hide");
+	    }
+    }else{
+    	teams.removeClass("non-search");
+    }
+}
